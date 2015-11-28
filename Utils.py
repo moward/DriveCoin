2b1c482e7f9efc58684770bb8364d8857e450863
Matt Howard <mhowar@seas.upenn.edu>
2015-11-28 17:07:51 -0500

    Solution set and tree generation mostly working
    Still needs significant testing
    Resizing of MMF still needs work to operate cross-platform

--- Utils.py
+++ Utils.py
@@ -10,8 +10,15 @@ def toHex(input):
 def strxor(a, b):
   return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b)])
 
+# inefficient but only used on small integer types in practice (<8 Bytes)
 def intToStr(x, len):
   if len == 0:
     return ''
   else:
     return intToStr(x >> 8, len - 1) + chr(x & 0xff)
+
+def strToInt(s):
+  if len(s) == 0:
+    return 0
+  else:
+    return ord(s[-1]) + 256 * strToInt(s[:-1])
